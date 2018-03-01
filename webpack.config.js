102d09ffa1b4b7d10fb0967e5346bfc298c3ae39
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-03-01 13:33:10 +0000

    very basic react set up, with minimum webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: __dirname,
+	devtool: 'inline-sourcemap',
+	entry: __dirname + '/src/index.js',
+	output: {
+		path: __dirname + '/build',
+		filename: 'app.min.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [{
+					loader: 'html-loader',
+					options: { minimize: true }
+				}]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: __dirname + '/public/index.html',
+			filename: 'index.html'
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'build'),
+		compress: true,
+		port: 8080
+	}
+};
