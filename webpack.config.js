251225b21b4f7d626af88adead75b9fe167c8359
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-03-02 16:18:11 +0000

    updated indentation from tabs/4 to spaces/2

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,40 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	context: __dirname,
-	devtool: 'inline-sourcemap',
-	entry: __dirname + '/src/index.js',
-	output: {
-		path: __dirname + '/build',
-		filename: 'app.[hash].bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [{
-					loader: 'html-loader',
-					options: { minimize: true }
-				}]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: __dirname + '/public/index.html',
-			filename: 'index.html'
-		})
-	],
-	devServer: {
-		contentBase: path.join(__dirname, 'build'),
-		compress: true,
-		port: 8080
-	}
+  context: __dirname,
+  devtool: 'inline-sourcemap',
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'app.[hash].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: { minimize: true }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: __dirname + '/public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 8080
+  }
 };
